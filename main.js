258da3ffe6ee8e76130d70f366d6ce13bddca0c5
Jack Lai <imjacklai@gmail.com>
2015-09-05 21:28:10 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,25 @@
+var app = require("app");
+var BrowserWindow = require("browser-window");
+var mainWindow = null;
+
+app.on("window-all-closed", function() {
+  if (process.platform != "darwin") {
+    app.quit();
+  }
+});
+
+app.on("ready", function() {
+  var screen = require("screen");
+  var size = screen.getPrimaryDisplay().workAreaSize;
+  mainWindow = new BrowserWindow({
+    width: size.width,
+    height: size.height,
+    center: true
+  });
+  mainWindow.loadUrl("file://" + __dirname + "/index.html");
+  mainWindow.openDevTools();
+
+  mainWindow.on("close", function() {
+    mainWindow = null;
+  });
+});
